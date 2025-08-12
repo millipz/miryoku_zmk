0d4214e8b2f5e36f5aefebe57baf8ccd43a977f6
Miles Phillips <miles.phillips@perspectum.com>
2025-08-12 16:21:24 +0100

    Add preonic and custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_COLEMAK
+#define MIRYOKU_TAP_COLEMAK
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_CLIPBOARD_MAC
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
